15693e5d2d6cf4078c86d70500ffb6800447e333
Zaenal Abidin <zaenal.abidin110206@gmail.com>
2026-03-06 06:30:21 +0700

    feat: implement data validation and serialization with modern pandas syntax

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -1,15 +1,18 @@
 import sys
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
+from books_recommender.components.stage_01_data_validation import DataValidation
 from books_recommender.exception.exception_handler import AppException
 
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
+        self.data_validation = DataValidation()
         
     def start_training_pipeline(self):
         try:
             self.data_ingestion.initiate_data_ingestion()
+            self.data_validation.initiate_data_validation()
         except Exception as e:
             raise AppException(e, sys) from e
 
