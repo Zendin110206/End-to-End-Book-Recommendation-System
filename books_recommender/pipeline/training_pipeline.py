4466977c94cabe94cd950b6059c0ea47ffa2199b
Zaenal Abidin <zaenal.abidin110206@gmail.com>
2026-03-06 10:52:20 +0700

    feat: implement model trainer component

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -2,6 +2,7 @@ import sys
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
 from books_recommender.components.stage_01_data_validation import DataValidation
 from books_recommender.components.stage_02_data_transformation import DataTransformation
+from books_recommender.components.stage_03_model_trainer import ModelTrainer
 from books_recommender.exception.exception_handler import AppException
 
 
@@ -10,6 +11,7 @@ class TrainingPipeline:
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
         self.data_transformation = DataTransformation()
+        self.model_trainer = ModelTrainer()
         
         
     def start_training_pipeline(self):
@@ -17,10 +19,6 @@ class TrainingPipeline:
             self.data_ingestion.initiate_data_ingestion()
             self.data_validation.initiate_data_validation()
             self.data_transformation.initiate_data_transformation()
+            self.model_trainer.initiate_model_trainer()
         except Exception as e:
             raise AppException(e, sys) from e
-
-
-
-    
-   
